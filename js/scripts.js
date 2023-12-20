085ed4ff031b2c3b84b0ce4482c8e2c98facff1f
Delonteee <davide.gasparotti@tutanota.com>
2023-12-20 16:38:10 +0100

    aggiunte bombe

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,8 @@ console.log('diffSelect', diffSelect, typeof diffSelect);
 const gridContainer = document.querySelector('.grid-container');
 console.log('gridContainer', gridContainer, typeof gridContainer);
 
+let punti = 0;
+
 startButton.addEventListener('click', function() {
 
     console.log(diffSelect.value);
@@ -14,33 +16,46 @@ startButton.addEventListener('click', function() {
 
     gridContainer.innerHTML = '';
 
+    //genera 16 numeri casuali con numero max = difficolta gioco
+    const numeriBombe = [];
+    while (numeriBombe.length < 16) {
+
+        let random = numRandom(1, diffSelect.value);
+
+        //non ci devono essere doppioni
+        if (!numeriBombe.includes(random)) {
+            numeriBombe.push(random);
+        }
+    }
+
+    console.log('numeriBombe', numeriBombe, typeof numeriBombe);
+
     for (let i = 1; i <= cellNumber; i++) {
         const cell = document.createElement('div');
-        cell.innerHTML = '<span>' + i + '</span>'; 
-        cell.classList.add('cell');
-
-        cell.classList.add('cell-' + cellNumber);
-    /*   
-        LA RIGA DI CODICE PRECEDENTE RIASSUME QUESTO IF ELSE
-    
-        if (cellNumber == 100) {
-            cell.classList.add('cell-100');
-        } else if (cellNumber == 81) {
-            cell.classList.add('cell-81');
-        } else if (cellNumber == 49) {
-            cell.classList.add('cell-49');
-        }
-    */
+        cell.innerHTML =  i ; 
+        cell.classList.add('cell');cell.classList.add('cell-' + cellNumber);
+   
 
         cell.addEventListener('click', function(){
-
             console.log('this', this, typeof this);
 
-            cell.classList.add('active');
-            console.log(cell.innerText);
+            const numeroCella = parseInt(this.innerHTML);
+            console.log('numeroCella', numeroCella, typeof numeroCella);
+
+            if(numeriBombe.includes(numeroCella)) {
+                this.classList.add('bomb');
+            } else {
+                this.classList.add('clicked');
+                punti++;
+            }
+
+            console.log('PUNTEGGIO', punti);
         })
 
         gridContainer.append(cell);
     }
 })
 
+function numRandom(min, max) {
+    return Math.floor(Math.random () * (max - min + 1))  + min;
+}
